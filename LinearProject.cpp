83edbc50f3f52f101a7c7dc3303f61a8d837b661
Jsorey4 <jsorey04@gmail.com>
2026-02-11 18:48:50 -0600

    First commit

---
+++ LinearProject.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+void createM1(int rows, int cols);
+void createM2(int rows, int cols);
+
+int main()
+{
+    // Variable initialization:
+    int m1_rows = 0, m1_cols = 0, m2_rows = 0, m2_cols = 0;
+
+    // Minimum size of each dimension
+    int rows = 2, cols = 2;
+
+    char symmetricResponse = 'N', redoRowsAndCols = 'N';
+
+    bool symmetricMatrix = false;
+
+    cout << "How many rows would you like in the first matrix?";
+    cin >> m1_rows;
+
+    cout << "How many columns would you like in the first matrix?";
+    cin >> m1_cols;
+
+    cout << "How many rows would you like in the second matrix?";
+    cin >> m2_rows;
+
+    cout << "How many columns would you like in the second matrix?";
+    cin >> m2_cols;
+
+    // // Probably going to need a decision like, "Which matrix do you want to be symmetric? First, second, or neither."
+    // cout << "Would you like for the matrix to be symmetric? Please enter 'Y' or 'N'.";
+    // cin >> symmetricResponse;
+
+    // // Find string function to make responses all upper case
+    // symmetricResponse.uppercase();
+    // // Need to test user input with a switch statement to ensure valid input.
+
+    // // Tests if a symmetric matrix can occur
+    // if (symmetricResponse == 'Y' && m1_rows != m1_cols)
+    // {
+    //     cout << "The matrix you entered cannot be a symmetric matrix.\n"
+    //          << "The matrix will need to be a square matrix (i.e. 2x2, 3x3, etc.)\n"
+    //          << "Currently your matrix is a " << m1_rows << 'x' << m1_cols << endl
+    //          << "Would you like to renter your rows, columns, or choose for this to not be a "
+    //             "symmetric matrix?\n";
+    // }
+
+    int seed = 0;         
+    srand(seed);
+
+    createM1(m1_rows, m1_cols);
+
+    cout << endl;
+    
+    createM2(m2_rows, m2_cols);
+
+    
+    
+
+    return 0;
+}
+
+// Each function will correspond to a certain matrix manipulation
+
+// Create the first matrix:
+void createM1(int rows, int cols)
+{
+    int **matrix1 = new int*[rows];
+
+    for(int i = 0; i < rows; i++)
+        matrix1[i] = new int[cols];
+
+    // Creating the elemnts to the array/matrix
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            
+            matrix1[i][j] = rand() % 10;
+            cout << setw(4) << matrix1[i][j]; // printing out the matrix 
+        }
+        cout << '\n';
+    }
+
+    // Preventing memory leakage
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] matrix1[i];
+    }
+    delete[] matrix1;
+}
+
+// Create the first matrix:
+void createM2(int rows, int cols)
+{
+    int **matrix2 = new int*[rows];
+
+    for(int i = 0; i < rows; i++)
+        matrix2[i] = new int[cols];
+
+    // Creating the elemnts to the array/matrix
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            
+            matrix2[i][j] = rand() % 10;
+            cout << setw(4) << matrix2[i][j]; // printing out the matrix 
+        }
+        cout << '\n';
+    }
+
+    // Preventing memory leakage
+    for (int i = 0; i < rows; i++)
+    {
+        delete[] matrix2[i];
+    }
+    delete[] matrix2;
+}
+
+
+// // Will create a symmetric matrix along the main diagonal.
+// void symMatrix()
+// {
+
+// }
+
+// void LU_Matrix(int arr[][])
+// {
+
+// }
+
+
+// void LDU_Matrix(int arr[][])
+// {
+
+// }
